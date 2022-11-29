8509616a655e590d3a105db6f12f242daa482173
Yasincoding <yasin52school@gmail.com>
2022-11-29 19:15:50 +0100

    een testimonial

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -12,27 +12,27 @@ export default function Testimonials() {
       desc:
         "Yasin is a highly motivated team player who is able to drill down complex problems into more simpler ones. Moreover, when he can’t solve a problem himself or within his team, he’s the one who takes the lead in finding expertise elsewhere.",
     },
-    {
-      id: 2,
-      name: "Alex Kalinski",
-      title: "Co-Founder of DELKA",
-      img:
-        "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "assets/labb.png",
-      desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
-      featured: true,
-    },
-    {
-      id: 3,
-      name: "Martin Harold",
-      title: "CEO of ALBI",
-      img:
-        "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      icon: "assets/gp.jpg",
-      desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
-    },
+    // {
+    //   id: 2,
+    //   name: "Alex Kalinski",
+    //   title: "Co-Founder of DELKA",
+    //   img:
+    //     "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    //   icon: "assets/labb.png",
+    //   desc:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+    //   featured: true,
+    // },
+    // {
+    //   id: 3,
+    //   name: "Martin Harold",
+    //   title: "CEO of ALBI",
+    //   img:
+    //     "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    //   icon: "assets/gp.jpg",
+    //   desc:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    // },
   ];
   return (
     <div className="testimonials" id="testimonials">
