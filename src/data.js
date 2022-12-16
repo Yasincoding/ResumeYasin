e7d9dedc65a4bceb672c4ef4fd3e91436427146a
Yasincoding <yasin52school@gmail.com>
2022-12-16 01:47:13 +0100

    Link to tcss

--- src/data.js
+++ src/data.js
@@ -1,101 +1,89 @@
 export const featuredPortfolio = [
-    {
-      id: 1,
-      title: "Coming soon!",
-      img:
-        "assets/react.png",
-    },
-    {
-      id: 1,
-      title: "Coming soon!",
-      img:
-        "assets/css.png",
-    },
-    {
-      id: 3,
-      title: "Coming soon!",
-      img: "assets/html.jpg",
-    }
-  ];
-  
-  export const webPortfolio = [
-    {
-      id: 1,
-      title: "Coming soon!",
-      img: "assets/js.png",
-    },
-    {
-      id: 3,
-      title: "Coming soon!",
-      img: "assets/ts.png",
-    },
-    {
-      id: 2,
-      title: "Coming soon!",
-      img: "assets/react.png",
-    },
-    {
-      id: 3,
-      title: "Coming soon!",
-      img: "assets/html.jpg",
-    },
-    {
-      id: 2,
-      title: "Coming soon!",
-      img: "assets/nextjs.jpeg",
-      link: "https://gemeente-rotterbosch-blog.netlify.app/"
-    },
-  ];
-  
-  export const mobilePortfolio = [
-    {
-      id: 1,
-      title: "Coming soon!",
-      img:
-        "assets/php.png",
-    },
-    {
-      id: 2,
-      title: "Coming soon!",
-      img:
-        "assets/mysql.png",
-    },
-    {
-      id: 3,
-      title: "Coming soon!",
-      img:
-        "assets/json.jpg",
-    },
-    {
-      id: 1,
-      title: "Coming soon!",
-      img:
-        "assets/contentful.png",
-        link: "https://gemeente-rotterbosch-blog.netlify.app/",
-    }
-  ];
-  
-  export const designPortfolio = [
-    {
-      id: 1,
-      title: "Coming soon!",
-      img:
-        "assets/css.png",
-    },
-    {
-      id: 2,
-      title: "Coming soon!",
-      img:
-        "assets/sass.png",
-    },
-    {
-      id: 1,
-      title: "Coming soon!",
-      img:
-        "assets/tailwind.png",
-    }
-    
-  ];
-  
+  {
+    id: 1,
+    title: "Coming soon!",
+    img: "assets/react.png",
+  },
+  {
+    id: 1,
+    title: "Coming soon!",
+    img: "assets/css.png",
+  },
+  {
+    id: 3,
+    title: "Coming soon!",
+    img: "assets/html.jpg",
+  },
+];
 
-  
+export const webPortfolio = [
+  {
+    id: 1,
+    title: "Coming soon!",
+    img: "assets/js.png",
+  },
+  {
+    id: 3,
+    title: "Coming soon!",
+    img: "assets/ts.png",
+  },
+  {
+    id: 2,
+    title: "Coming soon!",
+    img: "assets/react.png",
+  },
+  {
+    id: 3,
+    title: "Coming soon!",
+    img: "assets/html.jpg",
+  },
+  {
+    id: 2,
+    title: "Coming soon!",
+    img: "assets/nextjs.jpeg",
+    link: "https://gemeente-rotterbosch-blog.netlify.app/",
+  },
+];
+
+export const mobilePortfolio = [
+  {
+    id: 1,
+    title: "Coming soon!",
+    img: "assets/php.png",
+  },
+  {
+    id: 2,
+    title: "Coming soon!",
+    img: "assets/mysql.png",
+  },
+  {
+    id: 3,
+    title: "Coming soon!",
+    img: "assets/json.jpg",
+  },
+  {
+    id: 1,
+    title: "Coming soon!",
+    img: "assets/contentful.png",
+    link: "https://gemeente-rotterbosch-blog.netlify.app/",
+  },
+];
+
+export const designPortfolio = [
+  {
+    id: 1,
+    title: "Coming soon!",
+    img: "assets/css.png",
+  },
+  {
+    id: 2,
+    title: "Coming soon!",
+    img: "assets/sass.png",
+  },
+  {
+    id: 1,
+    title: "Coming soon!",
+    img: "assets/tailwind.png",
+    link: "https://gemeente-rotterbosch-blog.netlify.app/",
+  },
+];
